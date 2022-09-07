055598d1e08acfbb0a062ab1ae8b39922737f4d7
andompesta <ando.cavallari@gmail.com>
2022-09-07 12:06:22 +0200

    basic trainer

--- model_loader.py
+++ model_loader.py
@@ -1,9 +1,13 @@
 from src.utils import get_tokenizer
 from src.models import get_model
+import jax
 
 if __name__ == "__main__":
     tokenizer = get_tokenizer("distilroberta-base")
     model = get_model("distilroberta-base", num_labels=1)
-    model 
+    print(model.config)
+    rng = jax.random.PRNGKey(42)
+
+    params = model.init_weights(rng, model.input_shape)
 
 
