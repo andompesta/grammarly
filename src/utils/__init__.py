f3563b2a896b8edb0039dd8742b483810aa6b8f5
andompesta <ando.cavallari@gmail.com>
2022-09-12 18:41:01 +0200

    io utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,21 @@
 from .tokenizer import get_tokenizer
+from .io import (
+    ensure_dir,
+    save_obj_to_file,
+    load_obj_from_file,
+    load_data_from_json,
+    save_checkpoint,
+    save_data_to_json
+)
+
+
+
+__all__ = [
+    "get_tokenizer",
+    "ensure_dir",
+    "save_obj_to_file",
+    "load_obj_from_file",
+    "load_data_from_json",
+    "save_checkpoint",
+    "save_data_to_json"
+]
---
+++ src/utils/io.py
@@ -0,0 +1,37 @@
+from os import path, makedirs
+import pickle
+import json
+import torch
+import shutil
+
+from typing import Dict
+
+
+def ensure_dir(path_: str) -> str:
+    dir = path.dirname(path_)
+    if not path.exists(dir):
+        makedirs(dir)
+    return path_
+
+def save_obj_to_file(path_:str, obj:object):
+    with open(ensure_dir(path_), "wb") as writer:
+        pickle.dump(obj, writer)
+
+
+def load_obj_from_file(path_: str) -> object:
+    with open(path_, "rb") as reader:
+        obj = pickle.load(reader)
+    return obj
+
+def save_data_to_json(path_:str, data: object):
+    with open(ensure_dir(path_), "w", encoding="utf-8") as w:
+        json.dump(data, w, indent=2, sort_keys=True, default=lambda o: o.__dict__)
+
+def load_data_from_json(path_:str) -> object:
+    with open(path_, "r", encoding="utf-8") as r:
+        return json.load(r)
+
+def save_checkpoint(path_:str, state: Dict, is_best: bool, filename="checkpoint.pth.tar"):
+    torch.save(state, ensure_dir(path.join(path_, filename)))
+    if is_best:
+        shutil.copy(path.join(path_, filename), path.join(path_, "model_best.pth.tar"))
