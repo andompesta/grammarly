4e10ecd09a3f10d8acbf21fb4a2caca5c9e22949
andompesta <ando.cavallari@gmail.com>
2022-09-21 21:34:51 +0200

    update args to maximise gpu memroy

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--gradient_accumulation_steps", default=1, type=int)
     parser.add_argument("--unfreeze_layer", default=0, type=int)
     parser.add_argument("--batches_per_epoch", default=100, type=int)
-    parser.add_argument("--max_sentences_per_batch", default=300, type=int)
+    parser.add_argument("--max_sentences_per_batch", default=600, type=int)
     parser.add_argument("--max_tokens_per_batch", default=7000, type=int)
     parser.add_argument("--max_sentence_length", default=256, type=int)
     parser.add_argument("--num_workers", default=4, type=int)
@@ -99,114 +99,114 @@ if __name__ == "__main__":
     exp_name = f'{args.run_name}-{args.task_name}-{db_name}-{model_name}-{datetime.now().strftime("%d-%m-%y_%H-%M-%S")}'
     print(f"RUNNING EXPERIMENT -> {exp_name}")
 
-    with wandb.init(
-        project="grammarly",
-        name=exp_name,
-        job_type=args.task_name,
-        notes=args.notes,
-        config=vars(args),
-    ) as run:
+    # with wandb.init(
+    #     project="grammarly",
+    #     name=exp_name,
+    #     job_type=args.task_name,
+    #     notes=args.notes,
+    #     config=vars(args),
+    # ) as run:
 
 
-        # setup optimizers
-        named_params = list(model.named_parameters())
-        group_params = get_group_params(
-            named_params,
-            args.weight_decay,
-            no_decay=["bias", "layer_norm.weight", "layer_norm.bias"],
-        )
-        unfreeze_layer_params(named_params, layer=args.unfreeze_layer)
-        optim = get_optimizer(
-            method=args.optim_method,
-            params=group_params,
-            lr=args.lr,
-        )
-        scheduler = get_linear_scheduler_with_warmup(
-            optim, 
-            args.num_warmup_steps,
-            args.num_training_steps,
-        )
+    # setup optimizers
+    named_params = list(model.named_parameters())
+    group_params = get_group_params(
+        named_params,
+        args.weight_decay,
+        no_decay=["bias", "layer_norm.weight", "layer_norm.bias"],
+    )
+    unfreeze_layer_params(named_params, layer=args.unfreeze_layer)
+    optim = get_optimizer(
+        method=args.optim_method,
+        params=group_params,
+        lr=args.lr,
+    )
+    scheduler = get_linear_scheduler_with_warmup(
+        optim, 
+        args.num_warmup_steps,
+        args.num_training_steps,
+    )
 
-        model = model.to(device)
-        
-
-        if torch.cuda.device_count() > 1 and args.n_gpus > 1:
-            model = torch.nn.DataParallel(model, device_ids=[1, 0])
-            args.max_sentences_per_batch *= args.max_sentences_per_batch
-            args.max_tokens_per_batch *= (args.n_gpus // 2)
+    model = model.to(device)
     
-        best_f1 = 0.0
-        train_loss = []
-        train_acc = []
 
-        eval_loss = []
-        eval_prec = []
-        eval_recal = []
-        eval_f1 = []
-        eval_acc = []
+    if torch.cuda.device_count() > 1 and args.n_gpus > 1:
+        model = torch.nn.DataParallel(model, device_ids=[1, 0])
+        args.max_sentences_per_batch *= args.max_sentences_per_batch
+        args.max_tokens_per_batch *= (args.n_gpus // 2)
 
-        task = TokenClassificationTask(
-            name=exp_name,
-            args=args,
-            pad_token_id=model.config.pad_token_id,
+    best_f1 = 0.0
+    train_loss = []
+    train_acc = []
+
+    eval_loss = []
+    eval_prec = []
+    eval_recal = []
+    eval_f1 = []
+    eval_acc = []
+
+    task = TokenClassificationTask(
+        name=exp_name,
+        args=args,
+        pad_token_id=model.config.pad_token_id,
+    )
+
+    for epoch in range(1, args.epochs + 1):
+        train_iter_dl = train_epoch_gen.next_epoch_itr(shuffle=True)
+        loss, acc = task.train(
+            model=model,
+            optimizer=optim,
+            scheduler=scheduler,
+            dataloader=train_iter_dl,
+            device=device,
         )
 
-        for epoch in range(1, args.epochs + 1):
-            train_iter_dl = train_epoch_gen.next_epoch_itr(shuffle=True)
-            loss, acc = task.train(
+        train_loss.append(loss)
+        train_acc.append(acc)
+        print(f"epoch:{epoch}\tacc:{acc} \t loss:{loss}")
+        run.log(dict(train_loss=loss, train_accuracy=acc), step=epoch - 1)
+
+        if epoch % args.eval_every == 0 or epoch == 1:
+            is_best = False
+            eval_iter_dl = eval_epoch_gen.next_epoch_itr(shuffle=True)
+            scores = task.eval(
                 model=model,
-                optimizer=optim,
-                scheduler=scheduler,
-                dataloader=train_iter_dl,
+                dataloader=eval_iter_dl,
                 device=device,
             )
 
-            train_loss.append(loss)
-            train_acc.append(acc)
-            print(f"epoch:{epoch}\tacc:{acc} \t loss:{loss}")
-            run.log(dict(train_loss=loss, train_accuracy=acc), step=epoch - 1)
+            eval_loss = scores.get("eval_loss")
+            eval_acc = scores.get("eval_acc")
+            eval_prec = scores.get("eval_prec")
+            eval_rec = scores.get("eval_rec")
+            eval_f_score = scores.get("eval_f_score")
 
-            if epoch % args.eval_every == 0 or epoch == 1:
-                is_best = False
-                eval_iter_dl = eval_epoch_gen.next_epoch_itr(shuffle=True)
-                scores = task.eval(
-                    model=model,
-                    dataloader=eval_iter_dl,
-                    device=device,
+            print(
+                f"--------->eval\tacc:{eval_acc}\tloss{eval_loss}\tprec:{eval_prec}\trec:{eval_rec}\tf1:{eval_f_score}"
+            )
+            run.log(
+                scores,
+                step=epoch - 1,
+            )
+
+            if eval_f_score > best_f1:
+                best_f1 = eval_f_score
+                is_best = True
+
+            if isinstance(model, torch.nn.DataParallel):
+                state_dict = dict(
+                    [(n, p.to("cpu")) for n, p in model.module.state_dict().items()]
+                )
+            else:
+                state_dict = dict(
+                    [(n, p.to("cpu")) for n, p in model.state_dict().items()]
                 )
 
-                eval_loss = scores.get("eval_loss")
-                eval_acc = scores.get("eval_acc")
-                eval_prec = scores.get("eval_prec")
-                eval_rec = scores.get("eval_rec")
-                eval_f_score = scores.get("eval_f_score")
-
-                print(
-                    f"--------->eval\tacc:{eval_acc}\tloss{eval_loss}\tprec:{eval_prec}\trec:{eval_rec}\tf1:{eval_f_score}"
-                )
-                run.log(
-                    scores,
-                    step=epoch - 1,
-                )
-
-                if eval_f_score > best_f1:
-                    best_f1 = eval_f_score
-                    is_best = True
-
-                if isinstance(model, torch.nn.DataParallel):
-                    state_dict = dict(
-                        [(n, p.to("cpu")) for n, p in model.module.state_dict().items()]
-                    )
-                else:
-                    state_dict = dict(
-                        [(n, p.to("cpu")) for n, p in model.state_dict().items()]
-                    )
-
-                save_checkpoint(
-                    path_=os.path.join(
-                        args.ckp_path, args.task_name, db_name, model_name
-                    ),
-                    state=state_dict,
-                    is_best=is_best,
-                    filename=f"ckp_{epoch}.pth.tar",
-                )
+            save_checkpoint(
+                path_=os.path.join(
+                    args.ckp_path, args.task_name, db_name, model_name
+                ),
+                state=state_dict,
+                is_best=is_best,
+                filename=f"ckp_{epoch}.pth.tar",
+            )
