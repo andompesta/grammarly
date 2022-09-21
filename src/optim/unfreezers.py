d4ae3dbb0832c46a6e2cf82e10f63f5f0eb85937
andompesta <ando.cavallari@gmail.com>
2022-09-21 21:20:01 +0200

    update unfreeze function

--- src/optim/unfreezers.py
+++ src/optim/unfreezers.py
@@ -7,6 +7,12 @@ def unfreeze_layer_params(
     named_parameters: List[Tuple[str, torch.nn.Parameter]],
     layer: int = -1,
 ):
+    if layer == -1:
+        for n, p in named_parameters:
+            p.requires_grad = True
+            print(f"unfreeze -> {n}")
+        return
+
     for n, p in named_parameters:
         if "embed" in n:
             p.detach_()
@@ -14,7 +20,7 @@ def unfreeze_layer_params(
             print(f"FREEZE -> {n}")
         elif n.startswith("roberta.encoder.layer."):
             layer_number = int(n.split(".")[3])
-            if p.requires_grad and layer_number > layer:
+            if p.requires_grad and layer_number >= layer:
                 p.requires_grad = True
                 print(f"unfreeze -> {n}")
             else:
