b75295dcbed63200faebd7babafba0de206ce8be
andompesta <ando.cavallari@gmail.com>
2022-09-22 09:40:26 +0200

    implement concatenationof  datasets

--- src/utils/data/__init__.py
+++ src/utils/data/__init__.py
@@ -1,6 +1,9 @@
 from .omnitext_dataset import OmniDataset
 from .iterators import EpochBatchIterator
+from .concat_dataset import ConcatDataset
+
 __all__ = [
+    "ConcatDataset",
     "EpochBatchIterator",
     "OmniDataset",
 ]
