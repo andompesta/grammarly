284d610b208427b500f4c754e614c721ae8376a5
andompesta <ando.cavallari@gmail.com>
2022-09-24 11:59:25 +0200

    update imports

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,7 +1,9 @@
 from src.datasets.token_classification_dataset import (
-    get_token_classfication_dataset
+    get_token_classfication_dataset,
+    TokenClassificationDataset
 )
 
 __all__ = [
-    "get_token_classfication_dataset"
+    "get_token_classfication_dataset",
+    "TokenClassificationDataset"
 ]
